4fa544dd29201bfbfa652a71d6485f3eb5444691
Tom Usher <tom@tomusher.com>
2021-08-27 10:59:55 +0100

    Use WebGL backend, segment less frequently

--- main.js
+++ main.js
@@ -1,74 +1,83 @@
 const DEFAULT_SETTINGS = {
-  blurAmount: 3,
-  edgeBlurAmount: 10,
+    blurAmount: 3,
+    edgeBlurAmount: 10,
 };
 
-let model, video, canvas, settings;
+let model, video, canvas, settings, segmentation, lastSegmentation;
 
 const init = async () => {
-  model = await bodyPix.load();
-  video = await prepareWebcam();
-  video.height = video.videoHeight;
-  video.width = video.videoWidth;
-  video.play();
+    model = await bodyPix.load({
+        outputStride: 16,
+        multiplier: 0.5,
+    });
+    video = await prepareWebcam();
+    video.height = video.videoHeight;
+    video.width = video.videoWidth;
+    video.play();
 
-  canvas = document.getElementById("output");
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
+    canvas = document.getElementById("output");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
 
-  settings = getSettings();
+    settings = getSettings();
 
-  await doFrame();
+    await doFrame();
 };
 
 const getSettings = () => {
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const params = Object.fromEntries(urlSearchParams.entries());
-  return Object.assign(DEFAULT_SETTINGS, params);
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+    return Object.assign(DEFAULT_SETTINGS, params);
 };
 
 const prepareWebcam = async () => {
-  const video = document.getElementById("video");
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: {
-      facingMode: "user",
-      width: { ideal: 1280 },
-      height: { ideal: 720 },
-      frameRate: { ideal: 30, max: 30 },
-    },
-  });
-  video.srcObject = stream;
+    const video = document.getElementById("video");
+    const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+            facingMode: "user",
+            width: { ideal: 1280 },
+            height: { ideal: 720 },
+            frameRate: { ideal: 30, max: 30 },
+        },
+    });
+    video.srcObject = stream;
 
-  return new Promise((resolve) => {
-    video.onloadedmetadata = () => {
-      resolve(video);
-    };
-  });
+    return new Promise((resolve) => {
+        video.onloadedmetadata = () => {
+            resolve(video);
+        };
+    });
 };
 
-const doFrame = async () => {
-  const segmentation = await model.segmentPerson(video, {
-    flipHorizontal: false,
-    internalResolution: "low",
-    segmentationThreshold: 0.7,
-  });
-  const backgroundBlurAmount = parseInt(settings.blurAmount);
-  const edgeBlurAmount = parseInt(settings.edgeBlurAmount);
-  const flipHorizontal = false;
+const doFrame = async (now, metadata) => {
+    if (!metadata || (!lastSegmentation || lastSegmentation < metadata.presentationTime - 500)) {
+        if (metadata) {
+            lastSegmentation = metadata.presentationTime;
+        }
+        segmentation = await model.segmentPerson(video, {
+            flipHorizontal: false,
+            internalResolution: 0.5,
+            segmentationThreshold: 0.7,
+            maxDetections: 1,
+        });
+    }
+    const backgroundBlurAmount = parseInt(settings.blurAmount);
+    const edgeBlurAmount = parseInt(settings.edgeBlurAmount);
+    const flipHorizontal = false;
 
-  bodyPix.drawBokehEffect(
-    canvas,
-    video,
-    segmentation,
-    backgroundBlurAmount,
-    edgeBlurAmount,
-    flipHorizontal
-  );
+    bodyPix.drawBokehEffect(
+        canvas,
+        video,
+        segmentation,
+        backgroundBlurAmount,
+        edgeBlurAmount,
+        flipHorizontal
+    );
 
-  requestAnimationFrame(doFrame);
+    video.requestVideoFrameCallback(doFrame);
 };
 
 (async () => {
-  await init();
+    tf.setBackend("webgl").then(async () => await init());
 })();
